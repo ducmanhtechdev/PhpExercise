4a67fb10056e30ea41cbb21055e2153136f67485
Đức Mạnh <108124612+ducmanhtechdev@users.noreply.github.com>
2025-10-03 18:31:30 +0700

    btt5

--- BaiTapTuan5/delete_product.php
+++ BaiTapTuan5/delete_product.php
@@ -8,7 +8,7 @@ if (isset($_GET['id'])) {
     // Lấy thông tin sản phẩm từ cơ sở dữ liệu
     $productQuery = $conn->query("DELETE FROM products WHERE id = $productId");
      if ($productQuery) {
-        echo "<div class='alert alert-success'>Cập nhật thành công! 2s để chuyển hướng</div>";
+        echo "<div class='alert alert-success'>Xóa thành công! 2s để chuyển hướng</div>";
         header("Refresh:2; url=index.php");
     } else {
         echo "<div class='alert alert-danger'>Lỗi: " . $conn->error . "</div>";
